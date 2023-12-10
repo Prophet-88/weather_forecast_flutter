c49ddc2e50bc01d068ad8fcc545f47a5c5869e9c
Prophet-88 <a8lewis@gmail.com>
2023-12-10 21:26:22 +0900

    chore: 不要なフォルダの削除とcurrentWeather.dartの作成

---
+++ lib/page/currentWeather.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class currentWeather extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class currentWeatherState extends ConsumerState {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
